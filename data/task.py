1c46e5779140c77c7f8b0a6271c6424935b683da
Gian <giansantos.dev@gmail.com>
2024-02-07 02:21:54 -0300

    some modifications to tasks

--- data/task.py
+++ data/task.py
@@ -3,15 +3,54 @@ from datetime import datetime
 from pydantic import BaseModel, field_validator
 
 
+class Task(BaseModel):
+    id: int
+    name: str
+    description: str | None = None
+    done: bool
+    alert_date_time: datetime | None = None
+
+    @field_validator('alert_date_time')
+    def validate_alert_date_time(cls, value):
+        if isinstance(value, datetime):
+            return value
+        if isinstance(value, str):
+            return datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f')[:-3]
+        return None
+
+    @field_validator('done')
+    def validate_done(cls, value):
+        return bool(value)
+
+    @property
+    def alert_date_str(self) -> str | None:
+        if self.alert_date_time:
+            return self.alert_date_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        return None
+
+    @classmethod
+    def from_task_in_db(cls, task):
+        return cls(
+            id=task.id,
+            name=task.name,
+            description=task.description,
+            done=task.done,
+            alert_date_time=task.alert_date_time
+        )
+
+
 class TaskUpdate(BaseModel):
     id: int
     name: str | None = None
     description: str | None = None
     alert_date_time: datetime | None = None
+    done: bool | None = None
 
     @property
-    def alert_date_str(self) -> str:
-        return self.alert_date_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+    def alert_date_str(self) -> str | None:
+        if self.alert_date_time:
+            return self.alert_date_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        return None
 
 
 class TaskCreate(BaseModel):
@@ -34,32 +73,10 @@ class TaskCreate(BaseModel):
         return bool(value)
 
     @property
-    def alert_date_str(self) -> str:
-        return self.alert_date_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-
-
-class Task(BaseModel):
-    id: int
-    name: str
-    description: str | None = None
-    done: bool
-    alert_date_time: datetime | None = None
-
-    @field_validator('alert_date_time')
-    def validate_alert_date_time(cls, value):
-        if isinstance(value, datetime):
-            return value
-        if isinstance(value, str):
-            return datetime.strptime(value, '%Y-%m-%d %H:%M:%S.%f')[:-3]
+    def alert_date_str(self) -> str | None:
+        if self.alert_date_time:
+            return self.alert_date_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
         return None
-
-    @field_validator('done')
-    def validate_done(cls, value):
-        return bool(value)
-
-    @property
-    def alert_date_str(self) -> str:
-        return self.alert_date_time.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
 
 class TaskInDb(Task):
--- routers/task.py
+++ routers/task.py
@@ -3,22 +3,23 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException, status
 
-from data.task import TaskCreate
+from data.exceptions import TaskNotFoundException
+from data.task import TaskCreate, Task, TaskUpdate
 from data.user import User
-from dependencies import DB, get_current_user
+from dependencies import get_current_user, get_db
 
 router = APIRouter(
     prefix="/tasks",
-    tags=["tasks"]
+    tags=["tasks"],
 )
 
 
-@router.get("/tasks")
+@router.get("")
 async def get_user_tasks(
         current_user: Annotated[User, Depends(get_current_user)]
 ):
     try:
-        return DB.get_tasks_by_user_id(current_user.id)
+        return get_db().get_tasks_by_user_id(current_user.id)
     except ValueError as error:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -26,19 +27,71 @@ async def get_user_tasks(
         )
 
 
-@router.post('/tasks', status_code=status.HTTP_201_CREATED)
+@router.post('', status_code=status.HTTP_201_CREATED, response_model=Task)
 async def create_task(
         current_user: Annotated[User, Depends(get_current_user)],
         name: str,
-        description: str,
-        alert_datetime: datetime.datetime
+        description: str | None = None,
+        alert_datetime: datetime.datetime | None = None
 ):
     task_to_create = TaskCreate(name=name,
                                 description=description,
                                 alert_date_time=alert_datetime,
                                 user_id=current_user.id)
-    task_created = DB.create_task(task_to_create)
-    return {
-        'detail': 'Task created successfully',
-        'task': task_created.model_dump_json()
-    }
+    task_created = get_db().create_task(task_to_create)
+    return Task.from_task_in_db(task_created)
+
+
+@router.delete('', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(
+        current_user: Annotated[User, Depends(get_current_user)],
+        task_id: str
+):
+    try:
+        task = get_db().get_task_by_id(task_id)
+        if task.user_id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You don't have permission to delete this task."
+            )
+        get_db().delete_task(task_id)
+    except TaskNotFoundException as error:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(error)
+        )
+
+
+@router.put('', status_code=status.HTTP_200_OK, response_model=Task)
+async def update_task(
+        current_user: Annotated[User, Depends(get_current_user)],
+        task_id: int,
+        name: str | None = None,
+        description: str | None = None,
+        alert_datetime: datetime.datetime | None = None,
+        done: bool | None = None
+):
+    try:
+        task = get_db().get_task_by_id(task_id)
+        if task.user_id != current_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="You don't have permission to edit this task."
+            )
+
+        updated_task = get_db().update_task(
+            TaskUpdate(
+                id=task_id,
+                name=name,
+                description=description,
+                alert_date_time=alert_datetime,
+                done=done
+            )
+        )
+
+        return Task.from_task_in_db(updated_task).model_dump()
+    except TaskNotFoundException as error:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(error)
+        )
